04f3766b33e4c5628c55440d906e5be4ab81a8e0
Ali Haider Maqsood <alihaider.maqsood@gmail.com>
2024-05-24 17:16:44 +0200

    fixes

---
+++ Order.py
@@ -0,0 +1,6 @@
+class Order:
+    def __init__(self, description, quantity, price):
+        self.Description = description;
+        self.Quantity = "{:,.1f}".format(quantity);
+        self.Price = "{:,.2f}".format(price);
+        self.Total = "{:,.2f}".format(quantity * price);
