02b8d4939329f9413f69ece8a4d615cad8b5fda0
Ali Haider Maqsood <alihaider.maqsood@gmail.com>
2024-06-02 01:31:51 +0200

    testing library

--- PreventivoGenerator.py
+++ PreventivoGenerator.py
@@ -1,19 +1,19 @@
-import Order;
-import AlmaxGraphics.PdfManager as PM;
+from Classes.Order import Order;
+import AlmaxUtils.PdfManager as PM;
 
 def main():
     client_info = {
         "name": "Ali Srls"
     };
     orders = [
-        Order('fbewkjfbwebfjewbfkjewbfkewfewfvjewbfjwebfjkewjfbewfjwebjfwejfjwebfjewbfjwefwebfwefweeffwe', 2.6, 34.5),
+        Order('fbewkjfbwebfjewbfkjewbfkewfewfvjewbfjwebfjkewjfbewfjwebjfwejfjwebfjewbfjwefwebfwefweeffwe', 2.6, 31.5),
         Order('ciao', 2.6, 34090.5),
-        Order('fbewkjfbwebfjewbfkjewbfkewfewfvjewbfjwebfjkewjfbewfjwebjfwejfjwebfjewbfjwefwebfwefweeffwe', 2.6, 34.5),
-        Order('fbewkjfbwebfjewbfkjewbfkewfewfvjewbfjwebfjkewjfbewfjwebjfwejfjwebfjewbfjwefwebfwefweeffwe', 2.6, 34.5),
+        Order('fbewkjfbwebfjewbfkjewbfkewfewfvjewbfjwebfjkewjfbewfjwebjfwejfjwebfjewbfjwefwebfwefweeffwe', 2.6, 32.5),
+        Order('fbewkjfbwebfjewbfkjewbfkewfewfvjewbfjwebfjkewjfbewfjwebjfwejfjwebfjewbfjwefwebfwefweeffwe', 2.6, 33.5),
         Order('fbewkjfbwebfjewbfkjewbfkewfewfvjewbfjwebfjkewjfbewfjwebjfwejfjwebfjewbfjwefwebfwefweeffwe', 2.6, 34.5)
     ];
 
-    PM.PdfGenerator(client_info, orders);
+    PM.GeneratePdf(client_info, Order, orders);
 
 if __name__ == "__main__":
     main();
